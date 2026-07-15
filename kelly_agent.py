029862627747aa1c37b7d784577fcfb446a834e1
aleksanderbabicki-debug <aleksander.babicki@gmail.com>
2026-07-15 22:33:46 +0200

    Update kelly_agent.py

--- kelly_agent.py
+++ kelly_agent.py
@@ -5,9 +5,11 @@ import streamlit as st
 import asyncio
 import nest_asyncio
 
+
 # CRITICAL FIX: Naprawia błąd "There is no current event loop" w Streamlit Cloud
 nest_asyncio.apply()
 
+from langchain_google_generativeai import ChatGoogleGenerativeAI
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.agents import create_tool_calling_agent, AgentExecutor
 from langchain_core.prompts import ChatPromptTemplate
@@ -152,10 +154,11 @@ with st.sidebar:
 @st.cache_resource
 def init_agent(_api_key):
     os.environ["GOOGLE_API_KEY"] = _api_key
+     llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0)
 #    llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro-001", temperature=0)  # <-- ODKOMENTUJ TĘ LINIĘ
 #    llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash-latest", temperature=0)
 #    llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=0)
-    llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash-001", temperature=0)
+#    llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash-001", temperature=0)
 #    llm = ChatGoogleGenerativeAI(model="models/gemini-pro", temperature=0)
 #    llm = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0)
     prompt = ChatPromptTemplate.from_messages([
